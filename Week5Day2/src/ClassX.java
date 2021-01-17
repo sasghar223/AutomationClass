ae9ab3b37f74d9165b78f4914dbb5b0d3ff4ddc2
sasghar223 <76265036+sasghar223@users.noreply.github.com>
2021-01-17 13:38:34 -0500

    Week5day2

--- Week5Day2/src/ClassX.java
+++ Week5Day2/src/ClassX.java
@@ -4,5 +4,7 @@ public class ClassX  {
 	String i = "Ali";
 	String i1 = "Sam";
 	
-	
+	public void sample() {
+		System.out.println(i + "  "+i1);
+	}
 }
--- Week5Day2/src/ClassZ.java
+++ Week5Day2/src/ClassZ.java
@@ -4,6 +4,7 @@ public class ClassZ  {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ClassY Obj = new ClassY();
+		Obj.sample();
 		
 	}
 
